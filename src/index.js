41b221af1a6d4507ef02c289c6bfd85c0fbfcadb
Kazuki Nakamura <kajirikajiri@gmail.com>
2018-12-15 19:07:29 +0900

    move

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Radius(props) {
-  return (
-    <div className="radius">
-    </div>
-  )
-}
-
 function Name(props) {
   return (
     <div>
@@ -31,17 +24,39 @@ class Bye extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      x: 0, y: 0, countRadius: 0
+      x: 0, y: 0, countRadius: 0,click: false
     }
   }
 
-  onClick(e){
+  onMouseDown(e){
     const countRadius = this.state.countRadius + 1
     console.log("countRadius=" + countRadius)
     this.setState({
-      x:e.pageX, y: e.pageY, countRadius: countRadius
+      x:e.pageX, y: e.pageY, countRadius: countRadius, click: true
     })
-    console.log(e.pageX + ":" + e.pageY)
+    console.log("onMousedown" + e.pageX + ":" + e.pageY)
+  }
+
+  onMouseUp(e) {
+    this.setState({
+      x:e.pageX, y: e.pageY,click: false
+    })
+    console.log("mouseUP")
+  }
+
+  onMouseOver(e) {
+    if(this.state.click === true){
+      this.setState({
+        x:e.pageX, y: e.pageY
+      })
+    }
+    console.log("mouseover : " + e.pageX + ":" + e.pageY)
+  }
+
+  onDragEnd(e) { 
+    this.setState({
+    })
+    console.log("dragOff")
   }
 
   render(){
@@ -50,15 +65,15 @@ class Bye extends React.Component{
     const radiusStyle = {
       top: top,
       left: left,
-      position: "relative"
+      position: "relative",
+      transitionProperty: "all",
+      transitionDuration: "2s"
     }
     console.log(radiusStyle)
 
     return(
-      <div onClick={(e) => this.onClick(e)}>
-        <div className="screen" style={radiusStyle}>
-            <Radius/>
-        </div>
+      <div className="outer" onMouseDown={(e) => this.onMouseDown(e)} onMouseUp={(e) => this.onMouseUp(e)} onMouseOver={(e) => this.onMouseOver(e)} onDragEnd={(e) => this.onDragEnd(e)}>
+        <div className="screen" style={radiusStyle}/>
       </div>
     )
   }
